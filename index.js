5c33fb2e934652563bb5039f4026e24ef58135e2
aminulrony1999 <aminulrony1999@gmail.com>
2023-12-26 18:13:12 +0530

    user is added to the server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const users = [
+    {id : 1, name : 'Aminul', email : 'aminul@gmail.com'},
+    {id : 2, name : 'Rinku', email : 'rinku@gmail.com'},
+    {id : 1, name : 'Razon', email : 'razon@gmail.com'},
+    {id : 1, name : 'Mehedi', email : 'mehedi@gmail.com'}
+]
 app.get("/", (req, res) => {
   res.send("User managament server is running");
 });
-
+app.get("/users",(req,res) =>{
+    res.send(users);
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
   })
