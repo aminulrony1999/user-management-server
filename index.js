7edc5e68e680e4c57a759ef1dee784fe1f1a9d02
aminulrony1999 <aminulrony1999@gmail.com>
2023-12-26 18:56:45 +0530

    CORS as a middleware added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
 const users = [
     {id : 1, name : 'Aminul', email : 'aminul@gmail.com'},
     {id : 2, name : 'Rinku', email : 'rinku@gmail.com'},
