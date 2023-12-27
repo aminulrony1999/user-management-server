9b4910b46aa400d5a4a9ed5362086733f68ffff2
aminulrony1999 <aminulrony1999@gmail.com>
2023-12-27 18:03:51 +0530

    post api added

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.get("/", (req, res) => {
 app.get("/users",(req,res) =>{
     res.send(users);
 })
+app.post("/users",(req,res) =>{
+  console.log(req.body);
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
   })
