42cbb6435187bab847fb9a05619378e9c9bdc00e
aminulrony1999 <aminulrony1999@gmail.com>
2023-12-28 19:36:53 +0530

    data is send to server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.use(express.json());
 const users = [
     {id : 1, name : 'Aminul', email : 'aminul@gmail.com'},
     {id : 2, name : 'Rinku', email : 'rinku@gmail.com'},
@@ -18,6 +19,7 @@ app.get("/users",(req,res) =>{
     res.send(users);
 })
 app.post("/users",(req,res) =>{
+  console.log('post api hitting');
   console.log(req.body);
 })
 app.listen(port, () => {
